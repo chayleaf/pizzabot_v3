d04a76bce404239122ed1a8f558e8f624d0e2236
pavlukivan <ivan@pavluk.org>
2022-03-10 17:02:52 +0700

    make the bot case insensitive (hopefully)

--- pizzabot/src/lib.rs
+++ pizzabot/src/lib.rs
@@ -11,8 +11,9 @@ use std::{
 struct ChoiceMap<T: Default + Eq + Hash>(HashMap<T, usize>, usize);
 
 impl<T: Default + Eq + Hash> ChoiceMap<T> {
+    #[must_use]
     pub fn new() -> Self {
-        Default::default()
+        Self::default()
     }
     pub fn insert(&mut self, val: T) {
         *self.0.entry(val).or_insert(0) += 1;
@@ -53,42 +54,45 @@ pub struct Pizzabot {
 }
 
 impl Pizzabot {
+    #[must_use]
     pub fn new() -> Self {
-        Default::default()
+        Self::default()
     }
 
     pub fn set_message(&mut self, channel: &str, message: &str) {
         self.last_message.insert(channel.into(), message.into());
     }
 
+    /// # Panics
+    /// maybe
     pub fn add_message(&mut self, channel: &str, message: &str) {
         let last_message = self.last_message.get(channel).cloned();
-        if message.len() == 0 {
+        if message.is_empty() {
             return;
         }
         self.last_message.insert(channel.into(), message.into());
         let iter = message.split(' ');
-        let len = iter.clone().count();
-        if len < 1 {
-            return;
-        }
-        self.lengths.insert(len);
         let mut iter = iter.peekable();
-        let first_word = *iter.peek().unwrap();
+        let first_word = if let Some(first_word) = iter.peek() {
+            *first_word
+        } else {
+            return;
+        };
+        self.lengths.insert(iter.clone().count());
         if let Some(last_message) = last_message {
             let mut last_words = last_message.split(' ');
             if let Some(last_word) = last_words.next_back() {
-                let last_word2 = last_words.next_back();
+                let second_last_word = last_words.next_back();
                 self.first_words
-                    .entry(last_word.into())
+                    .entry(last_word.to_lowercase())
                     .or_insert_with(ChoiceMap::new)
-                    .insert((first_word.into(), last_word2.map(Into::into)));
+                    .insert((first_word.into(), second_last_word.map(str::to_lowercase)));
             }
         }
         let mut iter = message.split(' ').peekable();
         while let (Some(k), Some(&v)) = (iter.next(), iter.peek()) {
             self.words
-                .entry(k.into())
+                .entry(k.to_lowercase())
                 .or_insert_with(ChoiceMap::new)
                 .insert(v.into());
         }
@@ -118,23 +122,24 @@ impl Pizzabot {
 
     pub fn get_reply(&self, message: &str) -> Option<String> {
         let mut words = message.split(' ');
-        let last_word = words.next_back()?;
-        let last_word2 = words.next_back();
+        let last_word = words.next_back()?.to_lowercase();
+        let second_last_word = words.next_back().map(str::to_lowercase);
         let first_word = self
             .first_words
-            .get(last_word)
+            .get(&last_word)
             .and_then(|choices| {
-                if let Some(last_word2) = last_word2 {
-                    choices.choose_biased(|(_, word2)| {
-                        if word2.as_deref() == Some(last_word2) {
-                            4
-                        } else {
-                            1
-                        }
-                    })
-                } else {
-                    choices.choose()
-                }
+                second_last_word.map_or_else(
+                    || choices.choose(),
+                    |second_last_word| {
+                        choices.choose_biased(|(_, word2)| {
+                            if word2.as_deref() == Some(&second_last_word) {
+                                4
+                            } else {
+                                1
+                            }
+                        })
+                    },
+                )
             })?
             .0
             .as_str();
@@ -143,8 +148,8 @@ impl Pizzabot {
         let mut current = first_word;
         while let Some(word) = self
             .words
-            .get(&current.to_owned())
-            .and_then(|words| words.choose())
+            .get(&current.to_ascii_lowercase())
+            .and_then(ChoiceMap::choose)
         {
             current = word;
             words.push(current);
@@ -157,8 +162,8 @@ impl Pizzabot {
             if !Self::is_valid_end(current) {
                 if let Some(word) = self
                     .words
-                    .get(&current.to_owned())
-                    .and_then(|words| words.choose())
+                    .get(&current.to_ascii_lowercase())
+                    .and_then(ChoiceMap::choose)
                 {
                     current = word;
                     words.push(current);
@@ -170,6 +175,8 @@ impl Pizzabot {
         Some(words.join(" "))
     }
 
+    /// # Errors
+    /// yes
     pub fn load_legacy_file<S: AsRef<str>, P: AsRef<Path>>(
         &mut self,
         channel_id: S,
